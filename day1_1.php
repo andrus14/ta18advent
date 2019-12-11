9ab1a9fc3bae0676ae3a0c79e08c7b85964712c5
Andrus Naulainen <andrus.naulainen@gmail.com>
2019-12-11 14:20:29 +0200

    day1 done

--- day1_1.php
+++ day1_1.php
@@ -1,2 +1,12 @@
 <?php
-echo "hello\n";
+$file = fopen("day1.txt", "r");
+$floors = fgets($file);
+fclose($file);
+
+$down = substr_count($floors, '(');
+$up = substr_count($floors, ')');
+
+echo $down . "\n";
+echo $up . "\n";
+
+echo ($down - $up) . "\n";
